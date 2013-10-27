0be03fcef2f62d3f2097f1404d9b500a88c38735
ktom2135 <kedk2135@gmail.com>
2013-10-27 19:39:30 +0800

    Update Final project

--- Classes/GameLayer.h
+++ Classes/GameLayer.h
@@ -7,13 +7,81 @@
 USING_NS_CC;
 using namespace CocosDenshion;
 
-class GameLayer : public CCLayer {
+enum {
+    kSpriteBomb,
+    kSpriteShockwave,
+    kSpriteMeteor,
+    kSpriteHealth,
+    kSpriteHalo,
+    kSpriteSparkle
+};
+
+enum {
+    kBackground,
+    kMiddleground,
+    kForeground
+};
+
+
+class GameLayer : public CCLayer
+{
 private:
-	CCSize _screenSize;
-	CCSpriteBatchNode * _gameBatchNode;
+
+	CCArray * _meteorPool;
+    int _meteorPoolIndex;
+    
+    CCArray * _healthPool;
+    int _healthPoolIndex;
+    
+    
+	CCArray * _fallingObjects;
+    CCArray * _clouds;
+
+    CCSpriteBatchNode * _gameBatchNode;
+    CCSprite * _bomb;
+    CCSprite * _shockWave;
+    
+    CCSprite * _introMessage;
+    CCSprite * _gameOverMessage;
+    
+    CCLabelBMFont * _energyDisplay;
+    CCLabelBMFont * _scoreDisplay;
+    
+    CCAction * _growBomb;
+    CCAction * _rotateSprite;
+    CCAction * _shockwaveSequence;
+    CCAction * _swingHealth;
+
+    CCAction * _groundHit;
+    CCAction * _explosion;
+    
+    
+    CCSize _screenSize;
+	
+	float _meteorInterval;
+	float _meteorTimer;
+	float _meteorSpeed;
+    float _healthInterval;
+    float _healthTimer;
+    float _healthSpeed;
+    float _difficultyInterval;
+    float _difficultyTimer;
+
+	int _energy;
+    int _score;
+    int _shockwaveHits;
+    bool _running;
+    
+	void resetMeteor(void);
+    void resetHealth(void);
+    void resetGame(void);
+    void stopGame(void);
+    void increaseDifficulty(void);
     
     void createGameScreen (void);
-    
+    void createPools(void);
+    void createActions(void);
+	
 public:
     
     ~GameLayer(void);
@@ -26,6 +94,10 @@ public:
 
     // preprocessor macro for "static create()" constructor ( node() deprecated )
     CREATE_FUNC(GameLayer);
+    
+    void fallingObjectDone(CCNode* pSender);
+    void animationDone(CCNode* pSender);
+    void shockwaveDone();
     
     virtual void ccTouchesBegan(CCSet* pTouches, CCEvent* event);
    
